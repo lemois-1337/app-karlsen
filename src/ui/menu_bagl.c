b8edb78b220ee5fb7fc9ac9910edf7533735f9bb
Topper <Topper@somewhere.net>
2024-08-20 17:15:27 -0400

    Update Karlsen name

--- src/ui/menu_bagl.c
+++ src/ui/menu_bagl.c
@@ -30,7 +30,7 @@
 #include "../globals.h"
 #include "menu.h"
 
-UX_STEP_NOCB(ux_menu_ready_step, pnn, {&C_kaspa_logo, APPNAME, "is ready"});
+UX_STEP_NOCB(ux_menu_ready_step, pnn, {&C_karlsen_logo, APPNAME, "is ready"});
 UX_STEP_NOCB(ux_menu_version_step, bn, {"Version", APPVERSION});
 UX_STEP_CB(ux_menu_about_step, pb, ui_menu_about(), {&C_icon_certificate, "About"});
 UX_STEP_VALID(ux_menu_exit_step, pb, os_sched_exit(-1), {&C_icon_dashboard_x, "Quit"});
@@ -55,7 +55,7 @@ void ui_menu_main() {
     ux_flow_init(0, ux_menu_main_flow, NULL);
 }
 
-UX_STEP_NOCB(ux_menu_info_step, bn, {"Kaspa App", "(c) 2023 coderofstuff"});
+UX_STEP_NOCB(ux_menu_info_step, bn, {"Karlsen App", "(c) 2023 coderofstuff"});
 UX_STEP_CB(ux_menu_back_step, pb, ui_menu_main(), {&C_icon_back, "Back"});
 
 // FLOW for the about submenu:
